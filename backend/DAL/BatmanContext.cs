e6ec709a5a52b8ef44456055b0b29de591dcf3cc
Guillaume <gservais98@gmail.com>
2019-08-02 15:15:56 +0200

    update context

--- backend/DAL/BatmanContext.cs
+++ backend/DAL/BatmanContext.cs
@@ -6,12 +6,6 @@ namespace backend.DAL
 {
     public class BatmanContext : DbContext
     {
-        public BatmanContext(){}
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){}
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder){}
-
         //DB Set
         public DbSet<AcousticData> AcousticDatas {get;set;}
         public DbSet<Box> Boxes {get;set;}
@@ -19,6 +13,12 @@ namespace backend.DAL
         public DbSet<Operator> Operators {get;set;}
         public DbSet<Project> Projects {get;set;}
         public DbSet<Record> Records {get;set;}
+
+        public BatmanContext(DbContextOptions<BatmanContext> options):base(options){}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder){}
 
     }
 }
