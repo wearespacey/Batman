036ed36ad6d73b197d7144d7982659408e49b50e
xvercruysse <xavier.vercruysse@mic-belgique.be>
2019-08-03 11:06:48 +0200

    Added base map

--- BatmanUI/batman-ui/src/components/MapPage.tsx
+++ BatmanUI/batman-ui/src/components/MapPage.tsx
@@ -1,8 +1,46 @@
 import React, { Component } from 'react';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+
+type MapState = {
+    lat: number;
+    lng: number;
+    zoom: number;
+}
+
+class LeafletMap extends Component<{}, MapState> {
+  constructor(props:any) {
+    super(props)
+    this.state = {
+      lat: 50.606696,
+      lng: 3.511778,
+      zoom: 13
+    }
+  }
+
+  render() {
+    return (
+      <Map id="MapId" center={[this.state.lat, this.state.lng]} zoom={this.state.zoom}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        />
+        <Marker position={[this.state.lat, this.state.lng]}>
+          <Popup>
+            A pretty CSS3 popup. <br/> Easily customizable.
+          </Popup>
+        </Marker>
+      </Map>
+    );
+  }
+}
 
 class MapComponent extends Component {
     render() {
-        return <div className="AppContent MapPage">Map page</div>
+        return(
+            <div className="MapPage">
+                <LeafletMap/>
+            </div>
+        );
     }
 }
 
