ab1c6fad76f19a01c5067cbb6f83bbdfbd89ef4c
xvercruysse <xavier.vercruysse@mic-belgique.be>
2019-08-03 10:20:44 +0200

    Removed footer

--- BatmanUI/batman-ui/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Footer extends Component {
-    render() {
-        return <footer className="AppFooter">Batman Project - SpaceY - #hitw2019</footer>
-    }
-}
-
-export default Footer;
